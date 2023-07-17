be4128702a97fe803ffa88a1387eee3846e8065a
hwangsungjae <edx2fire@likelion.org>
2023-07-17 21:08:20 +0900

    chore : load assets

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts />
+    </Fragment>
+  )
+};
+
+export default HomePage;
